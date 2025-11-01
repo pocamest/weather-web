b0de4897dfb7ed48f70116e320cd4e5da88db694
pocamest <pocamest@yandex.ru>
2025-11-02 01:27:22 +0300

    test(auth): add registration test for invalid username

--- users/tests.py
+++ users/tests.py
@@ -7,6 +7,8 @@ from django.test import Client
 from django.urls import reverse
 from pytest_django.asserts import assertRedirects
 
+from .forms import RegistrationForm
+
 if TYPE_CHECKING:
     from .models import User
 
@@ -33,3 +35,23 @@ def test_successful_registration(client: Client) -> None:
     assert created_user is not None
     assert created_user.username == 'test_user'
     assert created_user.email == 'testuser@example.com'
+
+
+@pytest.mark.django_db
+def test_registration_with_invalid_username(client: Client) -> None:
+    user_data = {
+        'username': 'test@user',
+        'email': 'testuser@example.com',
+        'password1': 'testpassword',
+        'password2': 'testpassword',
+    }
+
+    url_registration = reverse('users:register')
+    response = client.post(path=url_registration, data=user_data)
+
+    assert UserModel.objects.count() == 0
+    assert response.status_code == 200
+
+    form: RegistrationForm = response.context['form']
+
+    assert form.has_error(field='username', code='invalid_username')
