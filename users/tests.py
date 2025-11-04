fd3301ac4ad491e6b12f281a3728ff00973c1479
pocamest <pocamest@yandex.ru>
2025-11-04 17:31:27 +0300

    refactor(tests): improve registration tests with fixture

--- users/tests.py
+++ users/tests.py
@@ -1,9 +1,11 @@
-from typing import TYPE_CHECKING
+from collections.abc import Callable
+from typing import TYPE_CHECKING, NotRequired, TypedDict, Unpack
 
 import pytest
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.exceptions import NON_FIELD_ERRORS
+from django.http import HttpResponse
 from django.test import Client
 from django.urls import reverse
 from pytest_django.asserts import assertRedirects
@@ -11,67 +13,85 @@ from pytest_django.asserts import assertRedirects
 from .forms import RegistrationForm
 
 if TYPE_CHECKING:
+    from django.test.client import _MonkeyPatchedWSGIResponse as DjangoTestResponse
+
     from .models import User
+else:
+    DjangoTestResponse = HttpResponse
 
 UserModel: type['User'] = get_user_model()
 
 
-@pytest.mark.django_db
-def test_successful_registration(client: Client) -> None:
-    user_data = {
-        'username': 'Test_user',
-        'email': 'TESTUSER@EXAMPLE.com',
-        'password1': 'testpassword',
-        'password2': 'testpassword',
-    }
-    url_registration = reverse('users:register')
-    response = client.post(path=url_registration, data=user_data)
+class UserRegistrationData(TypedDict):
+    username: str
+    email: str
+    password1: str
+    password2: str
 
+
+class UserRegistrationParams(TypedDict):
+    username: NotRequired[str]
+    email: NotRequired[str]
+    password1: NotRequired[str]
+    password2: NotRequired[str]
+
+
+RegisterUserCallable = Callable[[Unpack[UserRegistrationParams]], DjangoTestResponse]
+
+
+@pytest.fixture
+def register_user(
+    client: Client,
+) -> RegisterUserCallable:
+    def _register_user(**kwargs: Unpack[UserRegistrationParams]) -> DjangoTestResponse:
+        user_data: UserRegistrationData = {
+            'username': 'testuser',
+            'email': 'testuser@example.com',
+            'password1': 'testpassword',
+            'password2': 'testpassword',
+        }
+        user_data.update(kwargs)
+        url_registration = reverse('users:register')
+        return client.post(path=url_registration, data=user_data)
+
+    return _register_user
+
+
+@pytest.mark.django_db
+def test_successful_registration_normalizes_case(
+    register_user: RegisterUserCallable,
+) -> None:
+    response = register_user(username='TestUser', email='TestUser@example.com')
     assertRedirects(
         response=response, expected_url=str(settings.REGISTRATION_REDIRECT_URL)
     )
-
     assert UserModel.objects.count() == 1
     created_user = UserModel.objects.first()
     assert created_user is not None
-    assert created_user.username == 'test_user'
+    assert created_user.username == 'testuser'
     assert created_user.email == 'testuser@example.com'
 
 
 @pytest.mark.django_db
-def test_registration_with_invalid_username(client: Client) -> None:
-    user_data = {
-        'username': 'test@user',
-        'email': 'testuser@example.com',
-        'password1': 'testpassword',
-        'password2': 'testpassword',
-    }
-
-    url_registration = reverse('users:register')
-    response = client.post(path=url_registration, data=user_data)
-
+def test_registration_with_invalid_username(
+    register_user: RegisterUserCallable,
+) -> None:
+    response = register_user(username='test@user')
     assert UserModel.objects.count() == 0
     assert response.status_code == 200
-
     form: RegistrationForm = response.context['form']
 
     assert form.has_error(field='username', code='invalid_username')
 
 
 @pytest.mark.django_db
-def test_registration_with_duplicate_username(client: Client) -> None:
+def test_registration_with_duplicate_username(
+    register_user: RegisterUserCallable,
+) -> None:
     UserModel.objects.create_user(
-        username='TestUser', email='testuser@example.com', password='testpassword'
+        username='testuser', email='another@example.com', password='testpassword'
     )
-    user_data = {
-        'username': 'testuser',
-        'email': 'test@example.com',
-        'password1': 'testpassword',
-        'password2': 'testpassword',
-    }
-
-    url_registration = reverse('users:register')
-    response = client.post(path=url_registration, data=user_data)
+    response = register_user(username='TestUser')
 
     assert UserModel.objects.count() == 1
     assert response.status_code == 200
@@ -82,19 +102,13 @@ def test_registration_with_duplicate_username(client: Client) -> None:
 
 
 @pytest.mark.django_db
-def test_registration_with_duplicate_email(client: Client) -> None:
+def test_registration_with_duplicate_email(
+    register_user: RegisterUserCallable,
+) -> None:
     UserModel.objects.create_user(
-        username='testuser', email='testuser@example.com', password='testpassword'
+        username='anotheruser', email='testuser@example.com', password='testpassword'
     )
-    user_data = {
-        'username': 'testuser1',
-        'email': 'testuser@example.com',
-        'password1': 'testpassword',
-        'password2': 'testpassword',
-    }
-
-    url_registration = reverse('users:register')
-    response = client.post(path=url_registration, data=user_data)
+    response = register_user(email='TestUser@example.com')
 
     assert UserModel.objects.count() == 1
     assert response.status_code == 200
@@ -105,16 +119,10 @@ def test_registration_with_duplicate_email(client: Client) -> None:
 
 
 @pytest.mark.django_db
-def test_registration_with_different_passwords(client: Client) -> None:
-    user_data = {
-        'username': 'testuser',
-        'email': 'testuser@example.com',
-        'password1': 'testpassword1',
-        'password2': 'testpassword2',
-    }
-
-    url_registration = reverse('users:register')
-    response = client.post(path=url_registration, data=user_data)
+def test_registration_with_different_passwords(
+    register_user: RegisterUserCallable,
+) -> None:
+    response = register_user(password1='testpassword1', password2='testpassword2')
 
     assert UserModel.objects.count() == 0
     assert response.status_code == 200
