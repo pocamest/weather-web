98aa0c795a53c0892ee8b9858ea062d0e5ba6bd7
pocamest <pocamest@yandex.ru>
2025-11-05 08:27:42 +0300

    feat(auth): impelement home page placeholder for tests

--- config/urls.py
+++ config/urls.py
@@ -18,4 +18,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path('admin/', admin.site.urls), path('users/', include('users.urls'))]
+from users.views import home_placeholder_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+
+    path('', home_placeholder_view, name='home'),
+]
