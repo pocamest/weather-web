5ae0a5dc13b9a0a861bc4fa549c041b405e70ad4
pocamest <pocamest@yandex.ru>
2025-11-08 19:26:51 +0300

    fix(users): add full_clean validation to UserManager

--- users/models.py
+++ users/models.py
@@ -21,12 +21,18 @@ class UserManager(DjangoUserManager['User']):
 
         normalize_username = username.strip().lower()
         normalize_email = email.strip().lower()
-        return super().create_user(
-            username=normalize_username,
-            email=normalize_email,
-            password=password,
-            **extra_fields,
+
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+
+        user = self.model(
+            username=normalize_username, email=normalize_email, **extra_fields
         )
+        user.full_clean()
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
 
     def create_superuser(
         self,
@@ -35,16 +41,16 @@ class UserManager(DjangoUserManager['User']):
         password: str | None = None,
         **extra_fields: Any,
     ) -> 'User':
-        if not email:
-            raise ValueError('An email address must be provided.')
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
 
-        normalize_username = username.strip().lower()
-        normalize_email = email.strip().lower()
-        return super().create_superuser(
-            username=normalize_username,
-            email=normalize_email,
-            password=password,
-            **extra_fields,
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(
+            username=username, email=email, password=password, **extra_fields
         )
 
 
