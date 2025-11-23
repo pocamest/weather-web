fc50274532225597d2089e5d05618a6254f73f41
Rinat <pocamest@yandex.ru>
2025-11-23 22:16:20 +0300

    feat(locations): add pydantic schemas into API client
    * feat(locations): implement schemas by OpenWeatherAPI
    
    * feat(locations): add response validation to search_locations() in api client
    
    * feat(locations): add response validation to get_weather() in api client

--- locations/clients.py
+++ locations/clients.py
@@ -3,8 +3,10 @@ from typing import Any
 
 import requests
 from django.conf import settings
+from pydantic import ValidationError
 
 from .exceptions import APIError
+from .schemas import LocationSearchSchema, WeatherSchema, location_search_adapter
 
 
 class OpenWeatherClient:
@@ -28,9 +30,9 @@ class OpenWeatherClient:
             raise APIError(f'API request failed: {e}') from e
         return response.json()
 
-    def find_locations_by_name(
+    def search_locations(
         self, name: str, limit: int = settings.OPEN_WEATHER_DEFAULT_SEARCH_LIMIT
-    ) -> list[dict[str, Any]]:
+    ) -> list[LocationSearchSchema]:
         params: dict[str, str | int] = {
             'q': name,
             'limit': limit,
@@ -39,14 +41,20 @@ class OpenWeatherClient:
         response: list[dict[str, Any]] = self._execute_get_request(
             url=self.GEOCODING_URL, params=params
         )
-        return response
 
-    def find_weather_by_coordinates(
+        try:
+            locations_search = location_search_adapter.validate_python(response)
+        except ValidationError as e:
+            raise APIError(f'API request failed: {e}') from e
+
+        return locations_search
+
+    def get_weather(
         self,
         lat: Decimal,
         lon: Decimal,
         units: str = settings.OPEN_WEATHER_DEFAULT_UNITS,
-    ) -> dict[str, Any]:
+    ) -> WeatherSchema:
         params: dict[str, str] = {
             'lat': str(lat),
             'lon': str(lon),
@@ -56,4 +64,8 @@ class OpenWeatherClient:
         response: dict[str, Any] = self._execute_get_request(
             url=self.CURRENT_WEATHER_URL, params=params
         )
-        return response
+        try:
+            weather = WeatherSchema.model_validate(response)
+        except ValidationError as e:
+            raise APIError(f'API request failed: {e}') from e
+        return weather
---
+++ locations/schemas.py
@@ -0,0 +1,47 @@
+from decimal import Decimal
+
+from pydantic import (
+    AliasPath,
+    BaseModel,
+    ConfigDict,
+    Field,
+    TypeAdapter,
+    field_validator,
+)
+
+
+class LocationSearchSchema(BaseModel):
+    model_config = ConfigDict(str_strip_whitespace=True)
+
+    name: str
+    country_code: str = Field(max_length=2, validation_alias='country')
+    latitude: Decimal = Field(
+        max_digits=9, decimal_places=7, ge=-90, le=90, validation_alias='lat'
+    )
+    longitude: Decimal = Field(
+        max_digits=10, decimal_places=7, ge=-180, le=180, validation_alias='lon'
+    )
+
+    @field_validator('name')
+    @classmethod
+    def normalize_name(cls, v: str) -> str:
+        return v[:255]
+
+
+location_search_adapter = TypeAdapter(list[LocationSearchSchema])
+
+
+class WeatherSchema(BaseModel):
+    model_config = ConfigDict(str_strip_whitespace=True)
+
+    location_name: str = Field(validation_alias='name')
+    country_code: str = Field(
+        max_length=2, validation_alias=AliasPath('sys', 'country')
+    )
+    temperature: Decimal = Field(
+        max_digits=5,
+        decimal_places=2,
+        ge=-100,
+        le=100,
+        validation_alias=AliasPath('main', 'temp'),
+    )
