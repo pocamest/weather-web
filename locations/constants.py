2cf647397abe5f6a2d02aa6ac538474d9058ca02
pocamest <pocamest@yandex.ru>
2025-12-15 22:06:19 +0300

    refactor(locations): extract constant for api error message

--- locations/constants.py
+++ locations/constants.py
@@ -1,3 +1,4 @@
 MSG_WEATHER_API_CONNECTION_ERROR = 'Connection error, please try again later'
 MSG_ADD_LOCATION_FORBIDDEN = 'You must be logged in to add a location'
 MSG_ADD_LOCATION_BAD_REQUEST = 'Invalid request'
+LOG_MSG_API_ERROR = 'API call failed'
